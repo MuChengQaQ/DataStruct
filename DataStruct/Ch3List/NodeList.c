d45b690f1962dc77eb1c58b81d79b646f7056e6a
MuChengQaQ <1014880562@qq.com>
2026-03-15 15:39:27 +0800

    FixErrors

--- DataStruct/Ch3List/NodeList.c
+++ DataStruct/Ch3List/NodeList.c
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include <stdlib.h>
 
 #define State int
 #define ElemType int
@@ -15,20 +16,21 @@ typedef struct Node
 typedef Node * LinkList; //头指针类型
 
 //创建一个链表
-State creat_link_list(LinkList *l ,int n)
+State creat_link_list(LinkList *l ,int n)//n为全部节点数量，包含头节点
 {
 int j = 0 ;
 LinkList p,q;
 *l = (LinkList)malloc(sizeof(Node));
 (*l)->next = NULL;
 p = *l;
-while (j<n)
+while (j<n+1)
 {
-    j++;
-    p->data = j;
+    ++j;
+    
     q = (LinkList)malloc(sizeof(Node));
     q->next = p->next;
     p->next = q;
+    q->data = j-2;
 }
 
 return OK;
@@ -58,7 +60,7 @@ State seach_node (LinkList l , int n , ElemType *e)
         j++;
         p = p->next;
          
-        if (p->next = NULL)
+        if (p->next == NULL)
         {
             printf("输入位置超出链表长度\n");
             return ERROR;
@@ -75,20 +77,110 @@ State add_node (LinkList l , int n, ElemType e)
     int j = 0;
     LinkList p ,q;
     p = l ;
-    while (j < n)
+        while (j < n-1)
+        {
+            j++;
+            p = p->next;
+         
+            if (p->next == NULL)
+            {
+                printf("输入位置超出链表长度\n");
+                return ERROR;
+            }
+        }
+        q = (LinkList)malloc (sizeof (Node));
+        q->next = p->next ;
+        p->next = q ;
+        q->data = e;
+    return OK;
+}
+
+State delete_node (LinkList l , int n,ElemType *e) 
+{
+  
+    int j = 0;
+    LinkList p ,q;
+    p = l ;
+    while (j < n-1)
     {
         j++;
         p = p->next;
          
-        if (p->next = NULL)
+        if (p->next == NULL)
         {
             printf("输入位置超出链表长度\n");
             return ERROR;
         }
     }
-    q = (LinkList)malloc (sizeof (Node));
-    q->next = p->next ;
-    p->next = q ;
-    q->data = e;
-return OK;
+    q = p;
+    p = p->next ;
+    q->next = p->next;
+    *e = p->data;
+    free (p);
+    return OK;
+}
+
+State creat_circle_list (LinkList *l, int n)
+{
+    int j = 0 ;
+    LinkList p,q;
+    *l = (LinkList)malloc(sizeof(Node));
+    (*l)->next = NULL;
+    p = *l;
+    while (j<n)
+    {
+        j++;
+        p->data = j;
+        q = (LinkList)malloc(sizeof(Node));
+        q->next = p->next;
+        p->next = q;
+    }
+    q->next = *l ;
+
+    return OK;
+}
+
+int main (void)
+{   
+    printf("该程序为链表的相关程序，有生成，删除，插入，读取功能\n");
+    LinkList l;
+    ElemType e, e1=0;
+    creat_link_list (&l , 10 );
+   
+    for (size_t i = 0; i < 10; i++)
+    {
+        seach_node(l ,i,&e1);
+        printf("%d\n",e1);
+    }
+    
+
+    seach_node(l , 2 , &e);
+    seach_node(l , 11, &e);
+
+
+    add_node(l ,2,222);
+    for (size_t i = 0; i < 11; i++)
+    {
+        seach_node(l ,i,&e1);
+        printf("%d\n",e1);
+    }
+    add_node(l, 15, 2222);
+ 
+
+    delete_node(l ,2 , &e);
+    for (size_t i = 0; i < 10; i++)
+    {
+        seach_node(l ,i,&e1);
+        printf("%d\n",e1);
+    }
+    delete_node(l , 15, &e);
+   
+
+    delete_link_list(&l);
+    for (size_t i = 0; i < 10; i++)
+    {
+        seach_node(l ,i,&e1);
+        printf("%d\n",e1);
+    }
+return 0;
 }
